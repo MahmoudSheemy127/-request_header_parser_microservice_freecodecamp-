3af93a3be1f79a575e6d0de32057ae285d0cc3e6
MahmoudSheemy127 <mahmoudalshemy.3@gmail.com>
2022-07-03 20:34:18 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+
+//allow CORS
+app.use(cors())
+
+//send html page
+app.get('/',(req,res) => {
+    res.sendFile(__dirname + "/public/index.html")
+})
+
+//get 
+app.get('/api/whoami',(req,res) => {
+    console.log(req.headers)
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers["accept-language"],
+        software: req.headers["user-agent"]
+    })
+})
+
+
+
+app.listen(3000,() => {
+    console.log("Listening to port 3000")
+})
